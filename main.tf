0bae53b89bf5bf504ee206ee11ed3c8fcac9da04
malizkl <63977569+malizkl@users.noreply.github.com>
2021-11-24 22:16:03 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "google_container_cluster" "default" {
+  name        = var.name
+  project     = var.project
+  description = "Demo GKE Cluster"
+  location    = var.location
+
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
